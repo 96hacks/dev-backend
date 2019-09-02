69f753eb53d5437d907c693f0a548afc09f02ba1
krishna <contactkrishna139@gmail.comgit>
2019-09-02 23:40:08 +0530

    deleted description entity and added "data" in article

--- src/main/java/com/hacks/devbackend/model/Article.java
+++ src/main/java/com/hacks/devbackend/model/Article.java
@@ -1,15 +1,17 @@
 package com.hacks.devbackend.model;
 
 import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 @Entity
-@Table(name="article")
-public class Article{
+@Table(name = "article")
+public class Article {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "article_id", updatable = false, nullable = false)
@@ -19,42 +21,62 @@ public class Article{
 	private Date added_date;
 	private int likes;
 	private String background_image;
-	
+	private String data;
+
 	public int getArticle_id() {
 		return article_id;
 	}
+
 	public void setArticle_id(int article_id) {
 		this.article_id = article_id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCategory() {
 		return category;
 	}
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
 	public Date getAdded_date() {
 		return added_date;
 	}
+
 	public void setAdded_date(Date added_date) {
 		this.added_date = added_date;
 	}
+
 	public int getLikes() {
 		return likes;
 	}
+
 	public void setLikes(int likes) {
 		this.likes = likes;
 	}
+
 	public String getBackground_image() {
 		return background_image;
 	}
+
 	public void setBackground_image(String background_image) {
 		this.background_image = background_image;
 	}
-	
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
 }
